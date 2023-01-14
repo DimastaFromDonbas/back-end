b8ca42e0f1988f4365d511e30987e27a5c71cf31
DimastaFromDonbas <117586881+DimastaFromDonbas@users.noreply.github.com>
2023-01-14 14:42:44 +0200

    add back

---
+++ profi-backend-node-js-master/src/main.ts
@@ -0,0 +1,33 @@
+import {NestFactory} from "@nestjs/core";
+import {AppModule} from "./app.module";
+import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
+import {JwtAuthGuard} from "./auth/jwt-auth.guard";
+import {ValidationPipe} from "./pipes/validation.pipe";
+
+
+async function start() {
+    const express = require('express');
+    const cors = require('cors');
+    const path = require('path');
+    const PORT = process.env.PORT || 5000;
+    const app = await NestFactory.create(AppModule)
+
+    const config = new DocumentBuilder()
+        .setTitle('Урок по продвинотому BACKEND')
+        .setDescription('Документация REST API')
+        .setVersion('1.0.0')
+        .addTag('ULBI TV')
+        .build()
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('/api/docs', app, document);
+    
+    app.use(express.static(path.resolve(__dirname, 'build')));
+    app.use(cors());
+
+    app.useGlobalPipes(new ValidationPipe());
+
+    
+    await app.listen(PORT, () => console.log(`Server started on port = ${PORT}`))
+}
+
+start()
